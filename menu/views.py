6ea4f2820c1f0e5fe5ccb213a5b3887fe4072969
TomH131 <twhill2010@hotmail.co.uk>
2024-10-24 11:51:55 +0000

    adding menu app and html with basic look

--- menu/views.py
+++ menu/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello. Please take a look at our menu.")
+    return render(request, 'menu/menu.html')
