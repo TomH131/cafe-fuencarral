b59f94cd0ffeb6a6c3853cf863780050cf1013ce
TomH131 <twhill2010@hotmail.co.uk>
2024-10-24 15:33:38 +0000

    Adding anchor tags to the html to link the pages

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'reservations'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('reservations/', views.reservations_view, name='reservations'),
 ]
