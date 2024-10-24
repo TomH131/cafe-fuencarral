ba952995564ad0f3393beee98f6541165eeb8089
TomH131 <twhill2010@hotmail.co.uk>
2024-10-24 10:50:05 +0000

    adding menu app and migrating

--- cafefuencarral/urls.py
+++ cafefuencarral/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', include("reservations.urls")),
+    path('menu/', include("menu.urls")),
 ]
--- menu/views.py
+++ menu/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello. Please book a reservation.")
+    return HttpResponse("Hello. Please take a look at our menu.")
