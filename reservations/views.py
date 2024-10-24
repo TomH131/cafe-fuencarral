2a8d355e54165a29b1e14b702dbdd2723e63ce02
TomH131 <twhill2010@hotmail.co.uk>
2024-10-24 15:16:11 +0000

    Opening reservations.html and bringing across styling from menu page

--- reservations/views.py
+++ reservations/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello. Please book a reservation.")
+    return render(request, 'reservations/reservations.html')
