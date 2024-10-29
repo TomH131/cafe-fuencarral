a8dd0294e6bc9d169dc1a01a3743c3ee59914f8e
TomH131 <twhill2010@hotmail.co.uk>
2024-10-29 15:33:39 +0000

    Adding a model for reservation and setting up a superuser

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    # Define choices as a list of tuples
+    NUMBER_OF_PEOPLE_CHOICES = [
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+        (6, "6"),
+    ]
+
+    name = models.CharField(max_length=200)
+    people = models.IntegerField(
+        choices=NUMBER_OF_PEOPLE_CHOICES
+    )
+    date = models.DateField()
--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from reservations.models import Reservation
 
 # Create your views here.
 def reservations_view(request):
     return render(request, 'reservations/reservations.html')
+
+def Reservation(request):
+    class Meta:
+        model = Booking
+        fields = ['name', 'people', 'date']
