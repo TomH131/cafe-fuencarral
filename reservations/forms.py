d1bc8433a84d8417d3cdddc52bffd8d0379bfe63
TomH131 <twhill2010@hotmail.co.uk>
2024-11-08 14:29:12 +0000

    Split the form in two and over two different pages

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,46 +5,57 @@ from datetime import date
 
 MAX_RESERVATIONS_PER_SLOT = 10
 
-class ReservationForm(forms.ModelForm):
-    class Meta:
-        model = Reservation
-        fields = ['people', 'date', 'time', 'first_name', 'last_name', 'email']
-        widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
-        }
+class ReservationPart1Form(forms.Form):
+    people = forms.IntegerField(min_value=1, max_value=10, required=True)
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True)
+    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}), required=True)
 
-    def clean_date(self):
-        selected_date = self.cleaned_data['date']
-        if selected_date < date.today():
-            raise ValidationError("This date is in the past and cannot be selected")
-        return selected_date
+class ReservationPart2Form(forms.Form):
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
 
-    def clean(self):
-        cleaned_data = super().clean()
-        selected_date = cleaned_data.get('date')
-        selected_time = cleaned_data.get('time')
+
+# class ReservationForm(forms.ModelForm):
+#     class Meta:
+#         model = Reservation
+#         fields = ['people', 'date', 'time', 'first_name', 'last_name', 'email']
+#         widgets = {
+#             'date': forms.DateInput(attrs={'type': 'date'}),
+#             'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+#             'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+#             'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+#         }
+
+#     def clean_date(self):
+#         selected_date = self.cleaned_data['date']
+#         if selected_date < date.today():
+#             raise ValidationError("This date is in the past and cannot be selected")
+#         return selected_date
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         selected_date = cleaned_data.get('date')
+#         selected_time = cleaned_data.get('time')
     
-        if selected_date and selected_time:
-            formatted_date = selected_date.strftime('%d/%m/%Y')
+#         if selected_date and selected_time:
+#             formatted_date = selected_date.strftime('%d/%m/%Y')
             
-            formatted_time = selected_time.strftime('%H:%M')
+#             formatted_time = selected_time.strftime('%H:%M')
             
-            formatted_time = formatted_time.zfill(5)
+#             formatted_time = formatted_time.zfill(5)
             
-            existing_reservations = Reservation.objects.filter(
-                date=selected_date,
-                time=selected_time
-            ).count()
+#             existing_reservations = Reservation.objects.filter(
+#                 date=selected_date,
+#                 time=selected_time
+#             ).count()
         
-            if existing_reservations >= MAX_RESERVATIONS_PER_SLOT:
-                raise ValidationError(
-                    f"Sorry, all tables are booked for {formatted_date} at {formatted_time}. Please select a different time or date."
-                )
+#             if existing_reservations >= MAX_RESERVATIONS_PER_SLOT:
+#                 raise ValidationError(
+#                     f"Sorry, all tables are booked for {formatted_date} at {formatted_time}. Please select a different time or date."
+#                 )
     
-        return cleaned_data
+#         return cleaned_data
 
 class SearchForm(forms.Form):
     code = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Input your code'}))
--- reservations/views.py
+++ reservations/views.py
@@ -1,26 +1,86 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ReservationForm, SearchForm
+from .forms import ReservationPart1Form, ReservationPart2Form, SearchForm
 from .models import Reservation
-from datetime import datetime
+from django.http import HttpResponse, JsonResponse
+from datetime import datetime, date, time
 
-def reservations_view(request):
+# def reservations_view(request):
+#     if request.method == 'POST':
+#         form = ReservationForm(request.POST)
+#         if form.is_valid():
+#             reservation = form.save()
+#             return render(request, 'reservations/submission.html', {'code': reservation.code})
+#     else:
+#         form = ReservationForm()
+
+#     return render(request, 'reservations/reservations.html', {'form': form})
+
+# def submission_view(request):
+#     code = request.session.get('reservation_code')
+    
+#     if code:
+#         del request.session['reservation_code']
+
+#     return render(request, 'reservations/submission.html', {'code': code})
+
+def reservation_step1_view(request):
     if request.method == 'POST':
-        form = ReservationForm(request.POST)
+        form = ReservationPart1Form(request.POST)
         if form.is_valid():
-            reservation = form.save()
-            return render(request, 'reservations/submission.html', {'code': reservation.code})
-    else:
-        form = ReservationForm()
+            cleaned_data = form.cleaned_data
 
-    return render(request, 'reservations/reservations.html', {'form': form})
+            if cleaned_data.get('date'):
+                cleaned_data['date'] = cleaned_data['date'].strftime('%Y-%m-%d')
+
+            if cleaned_data.get('time'):
+                cleaned_data['time'] = cleaned_data['time'].strftime('%H:%M:%S')
+
+            request.session['reservation_data'] = cleaned_data
+
+            return redirect('reservations:step-two')
+
+    else:
+        form = ReservationPart1Form()
+
+    return render(request, 'reservations/reservation_step1.html', {'form': form})
+
+def reservation_step2_view(request):
+    reservation_data = request.session.get('reservation_data', {})
+
+    if request.method == 'POST':
+        form = ReservationPart2Form(request.POST)
+        if form.is_valid():
+            reservation_data.update(form.cleaned_data)
+            
+            reservation = Reservation.objects.create(
+                people=reservation_data['people'],
+                date=reservation_data['date'],
+                time=reservation_data['time'],
+                first_name=reservation_data['first_name'],
+                last_name=reservation_data['last_name'],
+                email=reservation_data['email']
+            )
+
+            del request.session['reservation_data']
+
+            request.session['reservation_code'] = reservation.code
+
+            return redirect('reservations:submission')
+    else:
+        form = ReservationPart2Form()
+
+    return render(request, 'reservations/reservation_step2.html', {'form': form})
+
 
 def submission_view(request):
     code = request.session.get('reservation_code')
-    
+
     if code:
         del request.session['reservation_code']
 
-    return render(request, 'reservations/submission.html', {'code': code})
+        return render(request, 'reservations/submission.html', {'code': code})
+
+    return redirect('reservations:reservations')
 
 def search_view(request):
     reservations = []
@@ -52,17 +112,50 @@ def modify_view(request, code):
     reservation = get_object_or_404(Reservation, code=code)
 
     if reservation.status != "Active":
-        return redirect('reservation:cancel', code=code)
+        return redirect('reservations:cancel', code=code)
 
     if request.method == 'POST':
-        form = ReservationForm(request.POST, instance=reservation)
-        if form.is_valid():
-            form.save()
-            return redirect('reservations:update', code=reservation.code)
-    else:
-        form = ReservationForm(instance=reservation)
+        form_part1 = ReservationPart1Form(request.POST, initial={
+            'people': reservation.people,
+            'date': reservation.date,
+            'time': reservation.time
+        })
+        form_part2 = ReservationPart2Form(request.POST, initial={
+            'first_name': reservation.first_name,
+            'last_name': reservation.last_name,
+            'email': reservation.email
+        })
 
-    return render(request, 'reservations/modify.html', {'form': form, 'reservation': reservation})
+        if form_part1.is_valid() and form_part2.is_valid():
+            reservation.people = form_part1.cleaned_data['people']
+            reservation.date = form_part1.cleaned_data['date']
+            reservation.time = form_part1.cleaned_data['time']
+            
+            reservation.first_name = form_part2.cleaned_data['first_name']
+            reservation.last_name = form_part2.cleaned_data['last_name']
+            reservation.email = form_part2.cleaned_data['email']
+            
+            reservation.save()
+
+            return redirect('reservations:update', code=reservation.code)
+
+    else:
+        form_part1 = ReservationPart1Form(initial={
+            'people': reservation.people,
+            'date': reservation.date,
+            'time': reservation.time
+        })
+        form_part2 = ReservationPart2Form(initial={
+            'first_name': reservation.first_name,
+            'last_name': reservation.last_name,
+            'email': reservation.email
+        })
+
+    return render(request, 'reservations/modify.html', {
+        'form_part1': form_part1,
+        'form_part2': form_part2,
+        'reservation': reservation
+    })
 
 def cancel_view(request, code):
     reservation = get_object_or_404(Reservation, code=code)
